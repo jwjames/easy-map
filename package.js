57c041c19bef4cf98d875413c9701ce1772c9803
Jeff James <jeffjames88@gmail.com>
2015-09-14 16:25:52 -0400

    edits

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jeffrey:easy-map',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'An extremely simple one-line Google Maps package.',
   // URL to the Git repository containing the source code for this package.
